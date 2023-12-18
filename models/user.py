b5710765c4aa6278a24e2978fff340ee33068afa
Mark Wonah <markwonah@gmail.com>
2023-12-18 15:30:03 +0100

    added User class for task 7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""
+User Class:
+    Inherits from BaseModel and Base
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
+class User(BaseModel, Base):
+    """
+    Represents users themselves, with personal information
+    """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place", cascade="delete", backref="user")
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        places = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializing"""
+        super().__init__(*args, **kwargs)
