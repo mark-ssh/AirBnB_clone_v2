c773abdaddbfc867f2df5cd8d82d97dec42661ba
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-18 11:43:05 -0800

    Edit the base model

--- models/base_model.py
+++ models/base_model.py
@@ -7,36 +7,39 @@ from sqlalchemy import Column, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
 
+
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     Base = declarative_base()
 else:
     Base = object
 
+
 class BaseModel:
     """The base class for all storage objects in this project"""
-    
+
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        id = Column(String(60), nullable=False, primary_key=True)
-        created_at = Column(DateTime(), nullable=False, default=datetime.now())
-        updated_at = Column(DateTime(), nullable=False, default=datetime.now())
+        id = Column(String(60), primary_key=True, nullable=False)
+        created_at = Column(DateTime(), default=datetime.utcnow(),
+                            nullable=False)
+        updated_at = Column(DateTime(), default=datetime.utcnow(),
+                            nullable=False)
 
     def __init__(self, *args, **kwargs):
         """initialize class object"""
-        if len(args) > 0:
-            for k in args[0]:
-                setattr(self, k, args[0][k])
+        if len(kwargs) > 0:
+            for k in kwargs.keys():
+                if k in ('created_at', 'updated_at'):
+                    setattr(self, k,
+                            datetime.strptime(kwargs[k],
+                                              '%Y-%m-%dT%H:%M:%S.%f'))
+                elif k == '__class__':
+                    continue
+                else:
+                    setattr(self, k, kwargs[k])
         else:
             self.created_at = datetime.now()
-            self.id = str(uuid.uuid4())
-
-        if kwargs is not None:
-            for k in kwargs.keys():
-                setattr(self, k, kwargs[k])
-        self.save()
-        """
-        for k in kwargs:
-            print("kwargs: {}: {}".format(k, kwargs[k]))
-        """
+            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
 
     def save(self):
         """method to update self"""
@@ -46,8 +49,8 @@ class BaseModel:
 
     def __str__(self):
         """edit string representation"""
-        return "[{}] ({}) {}".format(type(self)
-                                     .__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__)
 
     def to_dict(self):
         """convert to json"""
@@ -62,4 +65,4 @@ class BaseModel:
 
     def delete(self):
         """deleting current instance"""
-        models.storage.delete(self)
+        models.storage.__objects.pop(f"{type(self).__name__}.{self.id}")
