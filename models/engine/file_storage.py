41eec1111ab43eed6c4e91cd171b0f2f4b20225c
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-17 16:36:09 -0800

    Add delete functionality to file storage and allow for customized display of elements

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,8 +15,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage
+           Or optionally return only elements of a given class"""
+        if cls is not None:
+            cls_items = {}
+            for key, obj in FileStorage.__objects.items():
+                if isinstance(obj, cls):
+                    cls_items[key] = obj
+            if len(cls_items) > 0:
+                return cls_items
         return FileStorage.__objects
 
     def new(self, obj):
@@ -27,14 +42,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -45,6 +52,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            for key in self.__objects.keys():
+                if self.__objects[key] == obj:
+                    self.__objects.pop(key)
+                    break
