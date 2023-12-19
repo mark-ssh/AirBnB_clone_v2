01ef41e72ecb2fb6fc666e7019f5e9885e2207c4
Mark Wonah <markwonah@gmail.com>
2023-12-19 09:49:59 +0100

    updated task 9

--- models/review.py
+++ models/review.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import environ
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review class to represent user review"""
+    if environ.get("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "reviews"
+        text = Column(String(1024), 
+                      nullable=False)
+        place_id = Column(String(60), 
+                          ForeignKey("places.id"), 
+                          nullable=False)
+        user_id = Column(String(60), 
+                         ForeignKey("users.id"), 
+                         nullable=False)
+
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
+from datetime import datetime
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.__init__ import storage
+from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
+from console import HBNBCommand
 
 
-class test_review(test_basemodel):
-    """ """
+class Test_ReviewModel(unittest.TestCase):
+    """
+    Test the review model class
+    """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+    def setUp(self):
+        self.cli = HBNBCommand()
+        self.model = Review()
+        self.model.save()
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+    def tearDown(self):
+        self.cli.do_destroy("Review " + self.model.id)
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+    def test_var_initialization(self):
+        self.assertTrue(hasattr(self.model, "place_id"))
+        self.assertTrue(hasattr(self.model, "user_id"))
+        self.assertTrue(hasattr(self.model, "text"))
+        self.assertEqual(self.model.place_id, "")
+        self.assertEqual(self.model.user_id, "")
+        self.assertEqual(self.model.text, "")
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+if __name__ == "__main__":
+    unittest.main()
