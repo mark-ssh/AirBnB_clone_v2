3b49db429698d1aec51f9ddcfdba81fc6d191a3b
Mark Wonah <markwonah@gmail.com>
2023-12-19 10:09:27 +0100

    updated User and Place to include reviews class attribute

--- models/place.py
+++ models/place.py
@@ -2,6 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
 from os import environ
 
 
@@ -19,6 +20,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float)
         longitude = Column(Float)
+        reviews = relationship("Review", cascade="delete", backref="place")
     else:
         city_id = ""
         user_id = ""
