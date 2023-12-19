3898088dbc7b6d49b7075e8cb360987b5631b544
Mark Wonah <markwonah@gmail.com>
2023-12-19 11:22:14 +0100

    updated columns in task 10

--- models/place.py
+++ models/place.py
@@ -34,11 +34,11 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float)
         longitude = Column(Float)
-        reviews = relationship("Review", cascade="all, delete", 
-                               backref="place")
+        reviews = relationship("Review", cascade="delete", backref="place")
         amenities = relationship("Amenity",
-                                secondary='place_amenity',
-                                viewonly=False,
+                                 secondary='place_amenity',
+                                 viewonly=False,
+                                 backref="place_amenities")
     else:
         city_id = ""
         user_id = ""
@@ -51,28 +51,3 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
-    
-    def __init__(self, *args, **kwargs):
-        """Initializes the Place class"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def reviews(self):
-        """attribute for Filestorage that returns list of Review instances"""
-        values_review = models.storage.all("Review").values()
-        review_list = []
-        for review in values_review:
-            if review.place_id == self.id:
-                review_list.append(review)
-        return review_list
-
-    if environ.get('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def amenities(self):
-            """attribute for Filestorage that returns list of Amenity instances"""
-            values_amenity = models.storage.all("Amenity").values()
-            amenity_list = []
-            for amenity in values_amenity:
-                if amenity.place_id == self.id:
-                    amenity_list.append(amenity)
-            return amenity_list
