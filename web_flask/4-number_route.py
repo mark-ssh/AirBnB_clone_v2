f2cb2e84865308af87091a5d4f5387125c0a2055
Mark Wonah <markwonah@gmail.com>
2024-01-26 06:29:24 -0800

    added task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Very Simple Flask hello world
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+
+    """
+    Just returns text
+    """
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+
+    """
+    Returns the route /hbnb
+    """
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+
+    """
+    Returns the route /c/<text>
+    where <text> is any URL that the user requests
+    """
+    return ("C {:s}".format(text.replace("_", " ")))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>')
+def python_is(text="is cool"):
+
+    """
+    Returns the route /python/<text>
+    where <value> is any URI that the user requests
+    """
+    return ("Python {:s}".format(text.replace("_", " ")))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_is(n):
+
+    """
+    Displays /number/<n> URI only if it is an integer
+    """
+    return("{:d} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
